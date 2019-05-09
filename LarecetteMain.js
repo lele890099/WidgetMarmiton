66b6794fb7ac1480301681c1e6474bdc8eceb621
Léo Jacquier <47349829+lele890099@users.noreply.github.com>
2019-05-09 19:29:56 +0200

    Add files via upload

---
+++ LarecetteMain.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", event => {
+	window.Main = new Main();
+	window.Main.load();
+});
+
+class Main {
+	
+	constructor() {
+		trace("load dash");
+		this._name = "dash";
+	}
+	
+	load() {
+		let widget = new LaRecetteWidget(1, this);
+		document.body.appendChild(widget.mvc.view.stage);
+	}
+	
+	async get(url) {
+		return await Comm.urlrequest("https://node.nicopr.fr/" + this._name + url, {});
+	}
+	
+	async post(url, data) {
+		return await Comm.urlrequest("https://node.nicopr.fr/" + this._name + url, {"method": "POST", "data": data});
+	}
+	
+	get appname() {
+		return this._name;
+	}
+}
